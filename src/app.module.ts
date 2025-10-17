896da9b5676dd4b2249613ccc0211291fde594b0
Shahrukh Anwar <shahrukhanwar2013@gmail.com>
2025-10-17 20:20:06 +0530

    [UPDATED] - Final code changes merged

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AppDataSource } from './data-source';
 import { UserModule } from './user/user.module';
+import { CustomerModule } from './customer/customer.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
     TypeOrmModule.forFeature([User]),
     UserModule,
+    CustomerModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,12 @@ async function bootstrap() {
     });
 
   const app = await NestFactory.create(AppModule);
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:5173', // Replace with your frontend's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
